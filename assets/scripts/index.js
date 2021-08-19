ea74a6a954918e19642ef3df2e4edb1d2134ca21
Nicolas Melo <Nicolascmelo@hotmail.com>
2021-08-18 22:12:05 -0300

    em processo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function()){
+
 
 let addItem = function(){
 
@@ -23,38 +23,9 @@ $('.submission-line__btn').on('click', function(event){
   
   addItem();
 });
-$('.list').on('click', '.list__delete-btn', function(){
-  // removes that item from the list
-  $(this).parent().fadeOut(300, function(){
-    $(this).remove();
-  });
-});
 
-// CHECKING AN ITEM OFF FROM THE LIST
 
-// Clicking an item's check button:
-$('.list').on('click', '.list__check-btn', function(event){
-  // grays everything out
-  $(this).parent().toggleClass('list__item--checked');
-  $(this).siblings().toggleClass('list__delete-btn--checked');
-  $(this).toggleClass('list__check-btn--checked');
+//em andamento não consigui terminar no dia. 
 
-  // moves the element to either the bottom or top of the list
-  var $listItem = $(this).parent();
-  if ($listItem.hasClass('list__item--checked')) {
-    $('.list').append($listItem);
-  } else {
-    $('.list').prepend($listItem);
-  }
-});
-
-// MAKING LIST ITEMS SORTABLE
-
-$('.list').sortable({
-    distance: 2,
-    revert: 300,
-    cancel: ".list__item--checked"
-});
-
-};
+//aceito dicas 
 
